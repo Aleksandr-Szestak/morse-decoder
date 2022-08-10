41762d22127ba486c5c279c8057fdcc9b48ec36d
Aleksandr-Szestak <szestak@hotmail.com>
2022-08-10 12:26:47 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,49 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
+// --------------------------------------------------------
 function decode(expr) {
-    // write your solution here
+    let i,j;
+    let symb1, symb2, symb3;
+    let morzeStr, decodeS;
+    let rez = '';
+    
+    for(i=0;i<expr.length;i+=10){
+        symb1 = expr.slice(i,i+10);
+
+        if (symb1 === '**********'){
+            decodeS = ' ';
+        }
+        else{
+            morzeStr = '';
+            for(j=0;j<10;j+=2){
+                symb2 = symb1.slice(j,j+2);
+                //console.log(symb2);
+                if (symb2 === '11'){
+                    symb3 = '-';
+                }
+                else if (symb2 === '10'){
+                    symb3 = '.'
+                }
+                else{
+                    symb3 = '';
+                }
+                morzeStr += symb3;
+            }
+    
+            for (let key in MORSE_TABLE){
+                if (key === morzeStr){
+                    decodeS = MORSE_TABLE[key];
+                    break;
+                }
+            }      
+        }
+
+        rez += decodeS;
+    }
+
+    return rez;
 }
 
 module.exports = {
