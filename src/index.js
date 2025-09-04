a4e2e386322735ff5d227ba32228d3ac0dfc5373
Aleksandr-Szestak <szestak@hotmail.com>
2025-09-04 13:00:21 +0300

    feat: add implmentation of task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,39 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  let i;
+  let j;
+  let symb1;
+  let symb2;
+  let symb3;
+  let morzeStr;
+  let decodeS;
+  let rez = '';
+  for (i = 0; i < expr.length; i += 10) {
+    symb1 = expr.slice(i, i + 10);
+    if (symb1 === '**********') {
+      decodeS = ' ';
+    } else {
+      morzeStr = '';
+      for (j = 0; j < 10; j += 2) {
+        symb2 = symb1.slice(j, j + 2);
+        symb3 = '';
+        if (symb2 === '11') {
+          symb3 = '-';
+        } else if (symb2 === '10') {
+          symb3 = '.';
+        }
+        morzeStr += symb3;
+      }
+      if (morzeStr in MORSE_TABLE) {
+        decodeS = MORSE_TABLE[morzeStr];
+      } else {
+        decodeS = '';
+      }
+    }
+    rez += decodeS;
+  }
+  return rez;
 };
